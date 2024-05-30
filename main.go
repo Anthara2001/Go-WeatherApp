89c662c7515d47f344ea91f1b7b5de12ffbe473a
Anthara Gangone <anthara_gangone@epam.com>
2024-05-30 11:51:13 +0530

    changed the temp type from string to float64 in WeatherData struct

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ var openWeatherMapApiKey = "64d6472a8c3f759f80c62604cec8da06"
 type WeatherData struct {
 	City string `json:"name"`
 	Main struct {
-		Temp string `json:"temp_max"`
+		Temp float64 `json:"temp_max"`
 	} `json:"main"`
 }
 

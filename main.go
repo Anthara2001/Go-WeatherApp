ae2eecc78052f87fa690074dd740cfd054c8d72a
Anthara Gangone <anthara_gangone@epam.com>
2024-05-30 11:49:11 +0530

    Done with implementing the handler functions

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
+	"strings"
 )
 
-var openWeatherMapApi = "64d6472a8c3f759f80c62604cec8da06"
+var openWeatherMapApiKey = "64d6472a8c3f759f80c62604cec8da06"
 
 type WeatherData struct {
 	City string `json:"name"`
@@ -13,10 +15,28 @@ type WeatherData struct {
 	} `json:"main"`
 }
 
+func hello(res http.ResponseWriter, req *http.Request) {
+	res.Write([]byte("Welcome to WeatherApp by Golang"))
+}
+
+func query(city string) WeatherData {
+	var d WeatherData
+	resp, _ := http.Get("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + openWeatherMapApiKey)
+	json.NewDecoder(resp.Body).Decode(&d)
+	return d
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/hello", hello)
-	mux.HandleFunc("/weather/", func())
+	mux.HandleFunc("/weather/",
+
+		func(res http.ResponseWriter, req *http.Request) {
+			city := strings.SplitN(req.URL.Path, "/", 3)[2]
+			data := query(city)
+			json.NewEncoder(res).Encode(data)
+		},
+	)
 
 	s := &http.Server{
 		Addr:    ":8080",

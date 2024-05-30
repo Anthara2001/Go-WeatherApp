637b48cd3e6e20bb43f96545224293e23bca0e65
Anthara Gangone <anthara_gangone@epam.com>
2024-05-30 11:43:21 +0530

    Created the server and handler functions

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"net/http"
+)
+
 var openWeatherMapApi = "64d6472a8c3f759f80c62604cec8da06"
 
 type WeatherData struct {
@@ -7,4 +11,16 @@ type WeatherData struct {
 	Main struct {
 		Temp string `json:"temp_max"`
 	} `json:"main"`
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/hello", hello)
+	mux.HandleFunc("/weather/", func())
+
+	s := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	s.ListenAndServe()
 }
